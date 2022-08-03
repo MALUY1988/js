556bfdc27d3ee001dde3a0e7652935ff812b4b15
MALUY1988 <99687753+MALUY1988@users.noreply.github.com>
2022-08-03 21:12:40 +0300

    create JS

---
+++ app.js
@@ -0,0 +1,75 @@
+
+            // 1 Переменные
+        //variable   он же var (устаревший) 
+        // var name = 'Sasha' //устаревшый вариант
+        //camelCase первая маленькая следующее слово с большой
+        //const firstName = 'Sasha' //константа
+        //const lastname = 'Melnyk' //string //желательно не менять назание в константе
+        //let age = 33  // number //let можно менять на const
+        //const isProgrammer = true //boolean
+        //let i const переменные
+IDBFactory
+        //const _ = 'private'
+        //const $ = 'some value'
+        //const if = 'error' //ошибка
+        //const withNum5 = '5'
+        // const 5withNum = 5 //error
+InputDeviceInfo
+                    // 2 мутирование
+        //console.log('Имя человека: ' + firstName + ',а возраст: ' + age) //JavaScript приводит число к строке .toString()
+        //console.log(age) //в таком виде это число
+        //console.log  и alert в JavaScript отсутствуют!!!//
+        // prompt('Введите фамилию' ) //message не работает
+    // const lastName = prompt( message = 'Введите фамилию')
+        //alert(firstName + ' ' + lastName)  
+                        // 3 операторы
+    //let currentYear = 2022 // let i const не может быть вместе
+    //const birthYear = 1988 
+    //const age = currentYear - birthYear
+    if
+    //const a = 10
+    //const b = 5
+    //let c = 32
+    //c = c + a
+    //c += a // мы оcновываемся на предыдущем состоянии той же переменной мы можем ее заменить на такой синтаксис это касается  
+    //с-= a //                                                                                                             всех    
+    //c *= a //                                                                                                                математических 
+    //c /= a //
+    touchEvent                                                                                                                       операций
+    //console.log(a + b)
+    //console.log(a - b)
+    //console.log(a * b)
+    //console.log(a / b)
+    //console.log(currentYear++) // не будет так как currentYear константа //оператор ++ инкрементирует (добавляет единицу) в каждой новой строке
+    //console.log(++currentYear) // сначала выпоняется икрементирование потом выводится в console.log
+    // console.log(--currentYear) // оператор -- заберет единицу
+    //console.log(c)
+
+    //const не может быть пустой в отличии от  let!!!
+                //4 типы данных
+    //const isProgrammer = true
+        //const name = 'Sasha'
+        //const age = 26
+        //let x
+    // console.log(typeof isProgrammer) //boolean логический
+    // console.log(typeof name) //string строка
+    // console.log(typeof age) //number номер
+    // console.log(typeof x) // 4 примитив undefined
+    // console.log(typeof null) // 5 примитив null,null баг выводит object
+    //boolean string number undefined null примитивы!!!
+     
+      // 5 приоритет операторов
+      const fullAge = 33
+      const birthYear = 1988
+      const currentYear = 2022
+
+      //   > < >= <=  операторы отвечают за сравнение
+      const isFullAge = currentYear - birthYear >= fullAge //26 >= 27
+
+
+
+
+
+    
+
+   
